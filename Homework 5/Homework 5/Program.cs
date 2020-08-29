9f33526900669bcfb3e45cea364be82e5b4fcd31
Home PC <Hydral1sk89@gmail.com>
2020-08-29 22:58:59 +0300

    Complete task 1_2 and task 1_3

--- Homework 5/Homework 5/Program.cs
+++ Homework 5/Homework 5/Program.cs
@@ -49,75 +49,356 @@ namespace Example_005
 
     class Program
     {
-        // Задание 1.
-        // Воспользовавшись решением задания 3 четвертого модуля
-        // 1.1. Создать метод, принимающий число и матрицу, возвращающий матрицу умноженную на число
-        // 1.2. Создать метод, принимающий две матрицу, возвращающий их сумму
-        // 1.3. Создать метод, принимающий две матрицу, возвращающий их произведение
-
-        #region Задание_1
-        public static int[,] Task1_1 (int number, int[,] matrix)
+        #region Задание_1.1
+        /// <summary>
+        /// Метод возвращает матрицу умноженную на число
+        /// </summary>
+        /// <param name="number">число на которое будет умножена матрица</param>
+        /// <param name="matrix">матрица на которую будет умножено число</param>
+        /// <returns></returns>
+        public static int[,] Task1_1(int number, int[,] matrix)
         {
-                string strNumb;              
+            string strNumb;
 
-                bool isNumb = false;
+            bool isNumb = false;
 
-                string str;
+            string str;
 
-                //это количество строк в матрице
-                int strInt = matrix.GetLength(0);
+            //это количество строк в матрице
+            int strInt = matrix.GetLength(0);
 
-                bool isNum = false;
+            bool isNum = false;
 
-                string column;
+            string column;
 
-                //это количество столбцов в матрице
-                int columnInt = matrix.GetLength(1);
+            //это количество столбцов в матрице
+            int columnInt = matrix.GetLength(1);
 
-                bool isNumColumn = false;
+            bool isNumColumn = false;
 
-                int[,] arrayA = new int[strInt, columnInt];
-                int[,] arrayB = new int[strInt, columnInt];
+            int[,] arrayA = new int[strInt, columnInt];
+            int[,] arrayB = new int[strInt, columnInt];
 
-                //Умножаем матрицу на число и записываем результат в массив arrayB
-                for (int i = 0; i < strInt; i++)
+            //Умножаем матрицу на число и записываем результат в массив arrayB
+            for (int i = 0; i < strInt; i++)
+            {
+                for (int j = 0; j < columnInt; j++)
                 {
-                    for (int j = 0; j < columnInt; j++)
-                    {  
-                        arrayB[i, j] = matrix[i, j] * number;
-                    }
+                    arrayB[i, j] = matrix[i, j] * number;
+                }
+            }
+
+            //Вывод на экран
+            for (int i = 0; i < strInt; i++)
+            {
+                if (i == strInt / 2 && number < 10) Console.Write($"  {number} х |  ");
+                if (i == strInt / 2 && number > 9) Console.Write($" {number} х |  ");
+                if (i != strInt / 2) Console.Write("      |  ");
+
+                for (int k = 0; k < columnInt; k++)
+                {
+                    Console.Write(matrix[i, k]);
+                    Console.Write("  ");
                 }
 
-                //Вывод на экран
-                for (int i = 0; i < strInt; i++)
-                {
-                    if (i == strInt / 2 && number < 10) Console.Write($"  {number} х |  ");
-                    if (i == strInt / 2 && number > 9) Console.Write($" {number} х |  ");
-                    if (i != strInt / 2) Console.Write("      |  ");
+                if (i == strInt / 2) Console.Write("| = |  ");
+                else Console.Write("|   |  ");
 
-                    for (int k = 0; k < columnInt; k++)
+                for (int k = 0; k < columnInt; k++)
+                {
+                    Console.Write(String.Format("{0,3:0}", arrayB[i, k]));
+                    Console.Write("  ");
+                }
+                Console.Write("|");
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+            return arrayB;
+        }
+        #endregion
+
+        #region Задание_1.2
+        /// <summary>
+        /// Метод принимает две матрицы и в зависимости от знака складывает или вычитает матрицы
+        /// </summary>
+        /// <param name="matrixA">матрица А</param>
+        /// <param name="matrixB">матрица Б</param>
+        /// <param name="sign">знак сложения или вычитания (+ или -)</param>
+        /// <returns></returns>
+        public static int[,] Task1_2(int[,] matrixA, int[,] matrixB, char sign)
+        {
+            string str;
+
+            //Это количество строк в матрице
+            int strInt = matrixA.GetLength(0);
+
+            bool isNum = false;
+
+            string column;
+
+            //Это количество столбцов в матрице
+            int columnInt = matrixA.GetLength(1);
+
+            bool isNumColumn = false;
+
+            bool MinusOrPlus = false;
+            bool Plus = false;
+
+            if (sign == '+') Plus = true;
+            if (sign == '+' || sign == '-') MinusOrPlus = true;
+
+            int[,] matrixC = new int[strInt, columnInt];
+
+            //Складываем или вычитаем массивы и сохраняем результат в массив matrixC
+            for (int i = 0; i < strInt; i++)
+            {
+                for (int j = 0; j < columnInt; j++)
+                {
+
+
+                    if (Plus) matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+                    else matrixC[i, j] = matrixA[i, j] - matrixB[i, j];
+                }
+            }
+
+            Console.Clear();
+
+            //Вывод на экран
+            for (int i = 0; i < strInt; i++)
+            {
+                Console.Write("  |  ");
+
+                for (int k = 0; k < columnInt; k++)
+                {
+                    Console.Write(matrixA[i, k]);
+                    Console.Write("  ");
+                }
+
+                if (i == strInt / 2) Console.Write($"| {sign} |  ");
+                else Console.Write("|   |  ");
+
+                for (int k = 0; k < columnInt; k++)
+                {
+                    Console.Write(matrixB[i, k]);
+                    Console.Write("  ");
+                }
+                if (i == strInt / 2) Console.Write("| = |  ");
+                else Console.Write("|   |  ");
+
+                for (int k = 0; k < columnInt; k++)
+                {
+                    Console.Write(String.Format("{0,2:0}", matrixC[i, k]));
+                    Console.Write("  ");
+                }
+                Console.Write("|");
+
+                Console.WriteLine();
+            }
+
+            return matrixC;
+        }
+        #endregion
+
+        #region Задание_1.3
+        /// <summary>
+        /// Метод принимает две матрицы и возвращает их произведение
+        /// </summary>
+        /// <param name="matrixA">матрица А</param>
+        /// <param name="matrixB">матрица Б</param>
+        /// <returns></returns>
+        public static int[,] Task1_3(int[,] matrixA, int[,] matrixB)
+        {
+            //матрица1 строка
+            string str_matrix1;
+            int strInt_matrix1 = matrixA.GetLength(0);
+
+            //матрица1 столбцы
+            string column_matrix1;
+            int columnInt_matrix1 = matrixA.GetLength(1);
+
+            //матрица2 строка
+            string str_matrix2;
+            int strInt_matrix2 = matrixB.GetLength(0);
+
+            //матрица2 столбцы
+            string column_matrix2;
+            int columnInt_matrix2 = matrixB.GetLength(1);
+
+            if (columnInt_matrix1 != strInt_matrix2)
+            {
+                Console.WriteLine("Количество столбцов первой матрицы не равно количеству строк второй матрицы.\nТакие матрицы умножать нельзя!");
+                Console.ReadLine();
+            }
+
+            //Создаем массивы где будут храниться матрицы
+            int[,] matrix3 = new int[strInt_matrix1, columnInt_matrix2];
+
+            Console.Clear();
+
+            //Перемножаем матрицы
+            for (int i = 0; i < columnInt_matrix2; i++)
+            {
+                for (int j = 0; j < strInt_matrix1; j++)
+                {
+                    for (int q = 0; q < columnInt_matrix1; q++)
                     {
-                        Console.Write(matrix[i, k]);
+                        matrix3[j, i] += matrixA[j, q] * matrixB[q, i];
+                    }
+                }
+            }
+
+            //Вывод на экран
+            int height;
+
+            if (strInt_matrix1 < strInt_matrix2) height = strInt_matrix2;
+            else height = strInt_matrix1;
+            int matrix1_height = matrixA.GetLength(0);
+            int temp = height / 2 - matrix1_height / 2;
+            int matrix2_height = matrixB.GetLength(0);
+            int temp2 = height / 2 - matrix2_height / 2;
+            int matrix3_height = matrix3.GetLength(0);
+            int temp3 = height / 2 - matrix3_height / 2;
+
+            for (int i = 0; i < height; i++)
+            {
+                //Вывод матрицы1 если количество её строк меньше чем количество строк матрицы2
+                if (matrix1_height < height)
+                {
+                    if (i >= temp && matrix1_height > 0)
+                    {
+                        Console.Write("  |  ");
+
+                        for (int k = 0; k < columnInt_matrix1; k++)
+                        {
+                            Console.Write(matrixA[i - temp, k]);
+                            Console.Write("  ");
+                        }
+                        matrix1_height--;
+
+                        if (i != height / 2) Console.Write("|   |  ");
+                    }
+
+                    else
+                    {
+                        Console.Write("     ");
+
+                        for (int k = 0; k < columnInt_matrix1; k++)
+                        {
+                            Console.Write("   ");
+                        }
+
+                        if (i != height / 2) Console.Write("    |  ");
+                    }
+
+                    if (i == height / 2) Console.Write("| x |  ");
+                }
+
+                //Вывод матрицы1 если количество её строк такое же как количество строк матрицы2
+                if (matrix1_height == height)
+                {
+                    Console.Write("  |  ");
+
+                    for (int k = 0; k < columnInt_matrix1; k++)
+                    {
+                        Console.Write(matrixA[i, k]);
                         Console.Write("  ");
                     }
 
-                    if (i == strInt / 2) Console.Write("| = |  ");
-                    else Console.Write("|   |  ");
+                    if (i == height / 2) Console.Write("| x |  ");
+                    if (matrix1_height == matrix2_height && i != height / 2) Console.Write("|   |  ");
+                    if (matrix1_height > matrix2_height && i != height / 2) Console.Write("|      ");
 
-                    for (int k = 0; k < columnInt; k++)
+                }
+
+                //Вывод матрицы2 если количество её строк меньше чем количество строк матрицы1
+                if (matrix2_height < height)
+                {
+                    if (i >= temp2 && matrix2_height > 0)
                     {
-                        Console.Write(String.Format("{0,3:0}", arrayB[i, k]));
+                        for (int k = 0; k < columnInt_matrix2; k++)
+                        {
+                            Console.Write(matrixB[i - temp2, k]);
+                            Console.Write("  ");
+                        }
+                        matrix2_height--;
+
+                        if (i != height / 2) Console.Write("   ");
+                    }
+
+                    else
+                    {
+                        //Console.Write("     ");
+
+                        for (int k = 0; k < columnInt_matrix2; k++)
+                        {
+                            Console.Write("   ");
+                        }
+
+                        if (i != height / 2) Console.Write("   ");
+                    }
+
+                    if (i == height / 2) Console.Write("| =");
+                }
+
+                //Вывод матрицы2 если количество её строк такое же как количество строк матрицы2
+                if (matrix2_height == height)
+                {
+                    for (int k = 0; k < columnInt_matrix2; k++)
+                    {
+                        Console.Write(matrixB[i, k]);
                         Console.Write("  ");
+                    }
+
+                    if (i == height / 2) Console.Write("| =");
+                    else Console.Write("|  ");
+                }
+
+                //Вывод матрицы 3
+                if (matrix3_height == height)
+                {
+                    Console.Write(" | ");
+                    for (int k = 0; k < columnInt_matrix2; k++)
+                    {
+                        if (matrix3[i, k] > 0)
+                        {
+                            Console.Write(String.Format("{0,3:0}", matrix3[i, k]));
+                            Console.Write("  ");
+                        }
+                        else Console.Write("   ");
                     }
                     Console.Write("|");
-
-                    Console.WriteLine();
                 }
 
-                Console.ReadLine();
+                if (matrix3_height < height)
+                {
+                    if (i >= temp3 && matrix3_height > 0)
+                    {
+                        Console.Write(" | ");
+                        for (int k = 0; k < columnInt_matrix2; k++)
+                        {
+                            {
+                                Console.Write(matrix3[i - temp3, k]);
+                                 
+                                Console.Write(" ");
+                            }
+                        }
+                        matrix3_height--;
+                        Console.Write("|");
+                    }
+                }
 
-            return arrayB;           
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
+            return matrix3;
         }
+    
         #endregion
 
         #region Задание_2
@@ -307,20 +588,49 @@ namespace Example_005
 
         static void Main(string[] args)
         {
-            Random rnd = new Random();
 
-            int[,] m = new int[5, 5];
-            
+            ////Задание 1_2 и 1_3
+            //Random rnd = new Random();
 
-            for (int i = 0; i < m.GetLength(0); i++)
-            {
-                for (int k = 0; k < m.GetLength(1); k++)
-                {
-                    m[i, k] = rnd.Next(0, 5);
-                }
-            }
+            //int[,] m = new int[5, 5];
 
-            Task1_1(5, m);
+
+            //for (int i = 0; i < m.GetLength(0); i++)
+            //{
+            //    for (int k = 0; k < m.GetLength(1); k++)
+            //    {
+            //        m[i, k] = rnd.Next(0, 10);
+            //    }
+            //}
+
+            //int[,] n = new int[5, 5];
+
+
+            //for (int i = 0; i < m.GetLength(0); i++)
+            //{
+            //    for (int k = 0; k < m.GetLength(1); k++)
+            //    {
+            //        n[i, k] = rnd.Next(0, 5);
+            //    }
+            //}
+
+            //Task1_3(m, n);
+
+            //Задание 1_1
+            //Random rnd = new Random();
+
+            //int[,] m = new int[5, 5];
+
+
+            //for (int i = 0; i < m.GetLength(0); i++)
+            //{
+            //    for (int k = 0; k < m.GetLength(1); k++)
+            //    {
+            //        m[i, k] = rnd.Next(0, 5);
+            //    }
+            //}
+
+            //Task1_1(5, m);
 
 
 
