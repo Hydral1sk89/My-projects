fdaf0405bc331765ebfa637092b752e966169616
Hydral1sk89 <Hydral1sk89@gmail.com>
2020-09-02 13:56:59 +0300

    Optimize Task 2 on the advice of the teacher
    Use text.Split

--- Homework 5 Task 2/Homework 5 Task 2/Program.cs
+++ Homework 5 Task 2/Homework 5 Task 2/Program.cs
@@ -16,7 +16,6 @@ namespace Homework_5_Task_2
         // 1. Ответ: А
         // 2. ГГГГ, ДДДД
 
-        #region Task2
         /// <summary>
         /// Метод выводит на консоль самое длинное и самое короткое слово в тексте
         /// </summary>
@@ -24,58 +23,23 @@ namespace Homework_5_Task_2
         /// <returns></returns>
         public static string LongShortText(string text)
         {
-            StringBuilder sb = new StringBuilder();
-
-            int min = 0;
-            int max = 0;
-            int difference = 0;
-            int counter = 0;
-
-            string[] strArray = new string[text.Length];
-
-
-            for (int i = 0; i < text.Length; i++)
-            {
-                if (i == 0) min = i;
-                //if (i == text.Length - 1) max = i;                    
-                if (text[i] == ' ' || text[i] == '.' || text[i] == ',' || i == text.Length - 1)
-                {
-                    max = i;
-                    if (i == text.Length - 1) max = i + 1;
-                    difference = max - min;
-                    char[] word = new char[difference];
-
-                    for (int k = 0; k < difference; k++)
-                    {
-                        word[k] = text[min];
-                        min++;
-                    }
-
-                    foreach (char ch in word)
-                        sb.Append(ch);
-                    strArray[counter] = sb.ToString();
-                    counter++;
-                    min = max + 1;
-                    sb.Clear();
-                }
-            }
+            char[] cha = { ' ', '.', ',' };
+            string[] strArray = text.Split(cha, StringSplitOptions.RemoveEmptyEntries);
 
             string minStr = strArray[0];
             string maxStr = strArray[0];
             int StrLength;
 
-            for (int i = 0; i < text.Length; i++)
+            for (int i = 0; i < strArray.Length; i++)
             {
-                if (strArray[i] == null) break;
                 if (minStr.Length > strArray[i].Length && strArray[i] != "") minStr = strArray[i];
                 if (maxStr.Length < strArray[i].Length) maxStr = strArray[i];
             }
 
             StrLength = maxStr.Length;
 
-            for (int k = 0; k < text.Length; k++)
+            for (int k = 0; k < strArray.Length; k++)
             {
-                if (strArray[k] == null) break;
                 if (StrLength == strArray[k].Length && maxStr != strArray[k])
                 {
                     maxStr += " " + strArray[k];
@@ -87,14 +51,11 @@ namespace Homework_5_Task_2
 
             return "";
         }
-        #endregion
 
         static void Main(string[] args)
         {
-            //пробелы space
-            LongShortText("1 22 333 4444 55555 77777 88888 33333                         1111100000 CCCCC");
+            LongShortText("         Я пошёл гулять на хакатониндзеджимханувпермь      ");
 
-            //табуляция tab
             LongShortText("A 22 333 4444 55555 77777 88888 33333                            1111100000 CCCCCCCCCCCCCCCCCCCCCCC");
         }
     }
