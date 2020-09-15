532c40cd26cecb622bb38614123fc3f4d6689194
Home PC <Hydral1sk89@gmail.com>
2020-09-16 00:59:39 +0300

    I started working on redoing the task based on the teacher's comments

--- Homework 6 Task 1/Homework 6 Task 1/Program.cs
+++ Homework 6 Task 1/Homework 6 Task 1/Program.cs
@@ -148,11 +148,64 @@ namespace Homework_6_Task_1
             return numbers;
         }
 
+        static public void hakaton2(int[][] numbers)
+        {
+            int n = numbers[0].Length;
+
+            using (StreamWriter sw = new StreamWriter("hakaton.csv", true, Encoding.Unicode))
+            {
+                string group = string.Empty;
+
+                do
+                {
+
+
+
+                } while (n >= 0); // до тех пор пока не закончится массив
+            }
+        }
+
+        static public void StreamReadWrite()
+        {
+            using (StreamWriter sw = new StreamWriter("db.csv", true, Encoding.Unicode))
+            {
+                char key = 'д';
+
+                do
+                {
+                    string note = string.Empty;
+                    Console.Write("\nВведите имя автора записи: ");
+                    note += $"{Console.ReadLine()}\t";
+
+                    string now = DateTime.Now.ToShortTimeString();
+                    Console.WriteLine($"Время заметки {now}");
+                    note += $"{now}\t";
+
+                    Console.Write("Введите описание заметки: ");
+                    note += $"{Console.ReadLine()}\t";
+                    sw.WriteLine(note);
+                    Console.Write("Продожить н/д"); key = Console.ReadKey(true).KeyChar;
+                } while (char.ToLower(key) == 'д');
+            }
+
+            using (StreamReader sr = new StreamReader("db.csv", Encoding.Unicode))
+            {
+                string line;
+                Console.WriteLine($"{"Автор",15}{" Время",8} {"Описание"}");
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] data = line.Split('\t');
+                    Console.WriteLine($"{data[0],15}{data[1],8} {data[2]}");
+                }
+            }
+        }
+
 
         static void Main(string[] args)
         {
             //Создаем массив на N элементов
-            jaggedArray[0] = new int[50];
+            jaggedArray[0] = new int[10];
 
             //Поместим числа в основной массив
             PushNumb(jaggedArray[0]);
